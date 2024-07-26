b78a63ca99aed9b4a5d5c9a1e8bb21b7274ba973
BladeXses21 <bladexsesamv@gmail.com>
2024-07-26 20:08:05 +0300

    fix App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,13 @@ import React, { useEffect } from 'react';
 import { useTelegram } from './hooks/useTelegram';
 import Header from './components/Header/Header';
 
-const tg = window.Telegram.WebApp;
-
-
 function App() {
     const {onToggleButton, tg} = useTelegram();
 
 
     useEffect(() => {
         tg.ready();
-    })
+    }, [])
 
   return (
     <div className="App">
