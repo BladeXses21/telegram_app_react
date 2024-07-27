825396c1384c0b998ab1c3ab29f5c5334c67f456
BladeXses21 <bladexsesamv@gmail.com>
2024-07-27 16:07:04 +0300

    edit form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,9 +3,6 @@ import './Form.css';
 import { useTelegram } from '../../hooks/useTelegram';
 
 const Form = () => {
-//     const [name, setName] = useState('');
-//     const [lastname, setLastname] = useState('');
-//     const [email, setEmail] = useState('');
     const { tg } = useTelegram();
 
     useEffect(() => {
@@ -13,26 +10,6 @@ const Form = () => {
             text: 'send data'
         });
     }, []);
-
-    useEffect(() => {
-        if (!name || !lastname || !email) {
-            tg.MainButton.hide();
-        } else {
-            tg.MainButton.show();
-        }
-    }, [name, lastname, email]);
-
-//     const onChangeName = (e) => {
-//         setName(e.target.value);
-//     };
-//
-//     const onChangeLastname = (e) => {
-//         setLastname(e.target.value);
-//     };
-//
-//     const onChangeEmail = (e) => {
-//         setEmail(e.target.value);
-//     };
 
     const username = tg.initDataUnsafe?.user?.username || 'Username not available';
     const profilePhotoUrl = tg.initDataUnsafe?.user?.photo_url || 'https://via.placeholder.com/150';
@@ -44,9 +21,6 @@ const Form = () => {
                 <img src={profilePhotoUrl} alt="Profile" className="profile-photo" />
                 <span className={'username'}>{username}</span>
             </div>
-{/*             <input className={'input'} type='text' placeholder={'Name'} value={name} onChange={onChangeName} /> */}
-{/*             <input className={'input'} type='text' placeholder={'LastName'} value={lastname} onChange={onChangeLastname} /> */}
-{/*             <input className={'input'} type='text' placeholder={'Email'} value={email} onChange={onChangeEmail} /> */}
             <div className="amounts">
                 <div className="amount">
                     <label>Статок</label>
