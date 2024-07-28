d5a88fe2d2765d060e7b1fe5a47a8e7351a4dc53
BladeXses21 <bladexsesamv@gmail.com>
2024-07-28 21:57:34 +0300

    #73 edit user.js

--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = 'https://20be-91-245-124-201.ngrok-free.app';
+const apiUrl = 'https://3231-91-245-124-201.ngrok-free.app';
 
 const getUserData = async (telegramUserId) => {
     try {
