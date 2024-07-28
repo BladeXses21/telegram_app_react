38a423014ad06e2ceb898e0ef25c0508ad875a28
BladeXses21 <bladexsesamv@gmail.com>
2024-07-28 03:11:17 +0300

    edit file Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -11,34 +11,35 @@ const Form = () => {
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        const telegramId = tg.initDataUnsafe?.user?.id;
-        const fetchUserData = async (telegramId) => {
-            try {
-                const data = await getUserData(telegramId);
-                console.log('User data received:', data);
-                setUserId(data.uid);
-                // Fetch silver amount after getting userId
-                const balanceData = await getUserBalance(data.uid);
-                setSilverAmount(balanceData.silver || 0);
-            } catch (error) {
-                console.error('Error fetching user data:', error);
-                setError(error.message);
+        const fetchUserData = async () => {
+            const user = tg.initDataUnsafe?.user;
+            if (user && user.id) {
+                const telegramId = user.id;
+                try {
+                    const data = await getUserData(telegramId);
+                    console.log('User data received:', data);
+                    setUserId(data.uid);
+                    // Fetch silver amount after getting userId
+                    const balanceData = await getUserBalance(data.uid);
+                    setSilverAmount(balanceData.silver || 0);
+                } catch (error) {
+                    console.error('Error fetching user data:', error);
+                    setError(error.message);
+                    setUserId('Error');
+                    setSilverAmount('Error');
+                }
+
+                if (user.photo_url) {
+                    setProfilePhotoUrl(user.photo_url);
+                }
+            } else {
+                setProfilePhotoUrl('https://via.placeholder.com/150');
                 setUserId('Error');
                 setSilverAmount('Error');
             }
         };
 
-        const user = tg.initDataUnsafe?.user;
-        if (user && user.id) {
-            fetchUserData(user.id);
-            if (user.photo_url) {
-                setProfilePhotoUrl(user.photo_url);
-            }
-        } else {
-            setProfilePhotoUrl('https://via.placeholder.com/150');
-            setUserId('Error');
-            setSilverAmount('Error');
-        }
+        fetchUserData();
     }, [tg]);
 
     const username = tg.initDataUnsafe?.user?.username || 'Username not available';
