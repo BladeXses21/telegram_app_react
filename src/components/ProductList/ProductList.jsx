8781a0ecfcaf5ab68fe9ea8f63b1f469a411f05b
BladeXses21 <bladexsesamv@gmail.com>
2024-07-30 00:55:02 +0300

    #77 edit ProductList.jsx

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -11,43 +11,50 @@ const ProductList = () => {
     const [goldPrice, setGoldPrice] = useState('Loading...');
     const [userBaseRate, setUserBaseRate] = useState(1);
     const [sellPriceInSilver, setSellPriceInSilver] = useState('Loading...');
+    const [userId, setUserId] = useState(0);
 
 
     useEffect(() => {
         const fetchGoldAmount = async () => {
-            try {
-                const totalGoldData = await getTotalGold();
-                const currencyGoldData = await getCurrencyGold();
-                const exchangeRateData = await getExchangeRate();
+            const user = tg.initDataUnsafe?.user;
+            if (user && user.id) {
+                const telegramId = user.id;
+                try {
+                    setUserId(telegramId);
+                    const totalGoldData = await getTotalGold();
+                    const currencyGoldData = await getCurrencyGold();
+                    const exchangeRateData = await getExchangeRate();
 
-                const totalGold = parseFloat(totalGoldData.total_capacity);
-                const currencyGold = parseFloat(currencyGoldData.currency_capacity);
-                const rate = parseFloat(exchangeRateData.rate);
+                    const totalGold = parseFloat(totalGoldData.total_capacity);
+                    const currencyGold = parseFloat(currencyGoldData.currency_capacity);
+                    const rate = parseFloat(exchangeRateData.rate);
 
-                if (isNaN(totalGold) || isNaN(currencyGold) || isNaN(rate)) {
-                    throw new Error('Invalid data');
+                    if (isNaN(totalGold) || isNaN(currencyGold) || isNaN(rate)) {
+                        throw new Error('Invalid data');
+                    }
+
+                    const calculatedPrice = rate + 0.000000000001 * (totalGold / userBaseRate)
+                    setGoldPrice(calculatedPrice.toFixed(8)); // Округлюємо до 2 знаків після коми
+
+                    setGoldAmount(totalGold);
+                    setCurrencyGoldAmount(currencyGold);
+                    setExchangeRate(rate.toFixed(8));
+
+                    // Розрахунок ціни продажу золота за срібло
+                    if (userBaseRate) {
+                        const sellPrice = rate - 0.000000000001 * (totalGold / userBaseRate)
+                        setSellPriceInSilver(sellPrice.toFixed(8));
+                    }
+                } catch (error) {
+                    console.error('Error fetching data:', error);
+                    setGoldAmount('Error fetching data');
+                    setExchangeRate('Error fetching data');
+                    setGoldPrice('Error fetching data');
                 }
-
-                const calculatedPrice = rate + 0.000000000001 * (totalGold / userBaseRate)
-                setGoldPrice(calculatedPrice.toFixed(8)); // Округлюємо до 2 знаків після коми
-
-                setGoldAmount(totalGold);
-                setCurrencyGoldAmount(currencyGold);
-                setExchangeRate(rate.toFixed(8));
-
-                // Розрахунок ціни продажу золота за срібло
-                if (userBaseRate) {
-                    const sellPrice = rate - 0.000000000001 * (totalGold / userBaseRate)
-                    setSellPriceInSilver(sellPrice.toFixed(8));
-                }
-            } catch (error) {
-                console.error('Error fetching data:', error);
-                setGoldAmount('Error fetching data');
-                setExchangeRate('Error fetching data');
-                setGoldPrice('Error fetching data');
+            } else {
+                setUserId('Error');
             }
-        };
-
+        }
         fetchGoldAmount();
     }, [userBaseRate]);
 
@@ -65,18 +72,11 @@ const ProductList = () => {
 
     const handleBuyGold = async () => {
         try {
-            const user = tg.initDataUnsafe?.user;
-            if (user && user.id) {
-                const userUid = user.id;
-                amount = userBaseRate
-                const result = await buyGold(userUid, amount);
-                if (result.success) {
-                    await fetchGoldAmount();
-                }
-                alert('Successfully purchased.')
-            } else {
-                alert('User is not available.');
+            const result = await buyGold(userId, userBaseRate);
+            if (result.success) {
+                await fetchGoldAmount();
             }
+            alert('Successfully purchased.')
         } catch (error) {
             console.log('Error buying gold:', error);
             alert(`Error buying gold`)
