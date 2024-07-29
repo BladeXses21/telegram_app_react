aa4e625f5b01a6feae88b4d7a891e461eed29eea
BladeXses21 <bladexsesamv@gmail.com>
2024-07-29 23:43:03 +0300

    #76 edit ProductList and user.js

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css';
-import useBuyGold from '../../hooks/useBuyGold';
 import { useTelegram } from '../../hooks/useTelegram';
-import { getTotalGold, getExchangeRate, getCurrencyGold } from '../../api/user';
+import { getTotalGold, getExchangeRate, getCurrencyGold, updateExchangeRate, buyGold } from '../../api/user';
 
 const ProductList = () => {
     const { tg } = useTelegram();
@@ -12,10 +11,7 @@ const ProductList = () => {
     const [goldPrice, setGoldPrice] = useState('Loading...');
     const [userBaseRate, setUserBaseRate] = useState(1);
     const [sellPriceInSilver, setSellPriceInSilver] = useState('Loading...');
-    const [amountToBuy, setAmountToBuy] = useState(0);
-    const [amountToSell, setAmountToSell] = useState(0); // Додано для продажу золота
 
-    const { buyGold, loading: buyingGold, error: buyGoldError } = useBuyGold();  // Використовуємо хук для покупки золота
 
     useEffect(() => {
         const fetchGoldAmount = async () => {
@@ -33,16 +29,16 @@ const ProductList = () => {
                 }
 
                 const calculatedPrice = rate + 0.000000000001 * (totalGold / userBaseRate)
-                setGoldPrice(calculatedPrice.toFixed(10)); // Округлюємо до 2 знаків після коми
+                setGoldPrice(calculatedPrice.toFixed(8)); // Округлюємо до 2 знаків після коми
 
                 setGoldAmount(totalGold);
                 setCurrencyGoldAmount(currencyGold);
-                setExchangeRate(rate.toFixed(10));
+                setExchangeRate(rate.toFixed(8));
 
                 // Розрахунок ціни продажу золота за срібло
                 if (userBaseRate) {
                     const sellPrice = rate - 0.000000000001 * (totalGold / userBaseRate)
-                    setSellPriceInSilver(sellPrice.toFixed(10));
+                    setSellPriceInSilver(sellPrice.toFixed(8));
                 }
             } catch (error) {
                 console.error('Error fetching data:', error);
@@ -62,37 +58,42 @@ const ProductList = () => {
         }
     };
 
-    const handleAmountToBuyChange = (event) => {
-        setAmountToBuy(parseFloat(event.target.value));
-    };
-
-    const handleAmountToSellChange = (event) => {
-        setAmountToSell(parseFloat(event.target.value));
-    };
+    const handleButtonClick = () => {
+        handleBuyGold();
+        handleUpdateExchangeRate();
+    }
 
     const handleBuyGold = async () => {
         try {
             const user = tg.initDataUnsafe?.user;
             if (user && user.id) {
                 const userUid = user.id;
-                const result = await buyGold(userUid, amountToBuy);
-
+                const result = await buyGold(userUid, userBaseRate);
                 if (result.success) {
-                    // Оновлюємо дані після покупки
-                    await fetchGoldAmount(); // Переконайтеся, що ця функція визначена в компоненті
-                    alert('Gold purchased successfully!');
-                } else {
-                    alert('Failed to purchase gold.');
+                    await fetchGoldAmount();
                 }
+                alert('Successfully purchased.')
             } else {
-                alert('User ID is not available.');
+                alert('User is not available.');
             }
         } catch (error) {
-            console.error('Error buying gold:', error);
-            alert('Error buying gold.');
+            console.log('Error buying gold:', error);
+            alert('Error buying gold:', error)
         }
     };
 
+    const handleUpdateExchangeRate = async () => {
+        try {
+            const result = await updateExchangeRate(exchangeRate);
+            if (result.success) {
+                await fetchGoldAmount();
+            }
+            alert('Exchange rate successfully updated')
+        } catch (error) {
+            console.log('Error update exchange rate:', error)
+            alert('Error update exchange rate:', error)
+        }
+    };
 
     const handleSellGold = () => {
         console.log('Sell gold clicked with amount:', amountToSell);
@@ -124,29 +125,19 @@ const ProductList = () => {
                 </div>
             </div>
             <div className="input-container">
-                <label>Enter the amount of gold to buy:</label>
+                <label>Enter the amount of gold to buy or sell:</label>
                 <input
                     type="number"
-                    value={amountToBuy}
-                    onChange={handleAmountToBuyChange}
+                    value={userBaseRate}
+                    onChange={handleSellOrBuyChange}
                     step="0.1"
                     min="0"
                 />
-                <button onClick={handleBuyGold} className="buy-button" disabled={buyingGold}>
-                    {buyingGold ? 'Buying...' : 'Buy Gold'}
+                <button onClick={handleButtonClick} className="buy-button">
+                    Buy Gold
                 </button>
-                {buyGoldError && <div className="error-message">{buyGoldError}</div>}
-
-                <label>Enter the amount of gold to sell:</label>
-                <input
-                    type="number"
-                    value={amountToSell}
-                    onChange={handleAmountToSellChange}
-                    step="0.1"
-                    min="0"
-                />
                 <button onClick={handleSellGold} className="sell-button">
-                    Sell Gold
+                    Buy Gold
                 </button>
             </div>
         </div>
